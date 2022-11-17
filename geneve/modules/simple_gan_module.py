61686895e0e458d0735fc9e708cf5af78a2e90b3
Serge Kim <sergevkim@gmail.com>
2022-11-17 12:29:37 +0300

    forgotten detach

--- geneve/modules/simple_gan_module.py
+++ geneve/modules/simple_gan_module.py
@@ -42,6 +42,17 @@ class SimpleGANModule(pl.LightningModule):
         ones = torch.ones(images.shape[0], 1).to(self.device)
         zeros = torch.zeros(images.shape[0], 1).to(self.device)
 
+        # discriminator
+        d_real_outputs = self.discriminator(images)
+        real_loss = self.adversarial_criterion(d_real_outputs, ones)
+        d_fake_outputs = self.discriminator(generated_images.detach())
+        fake_loss = self.adversarial_criterion(d_fake_outputs, zeros)
+        d_loss = real_loss + fake_loss
+        self.log('d_loss', d_loss)
+        d_opt.zero_grad()
+        self.manual_backward(d_loss)
+        d_opt.step()
+
         # generator
         d_outputs = self.discriminator(generated_images)
         g_loss = self.adversarial_criterion(d_outputs, ones)
@@ -49,17 +60,6 @@ class SimpleGANModule(pl.LightningModule):
         g_opt.zero_grad()
         self.manual_backward(g_loss)
         g_opt.step()
-
-        # discriminator
-        d_real_outputs = self.discriminator(images)
-        real_loss = self.adversarial_criterion(d_real_outputs, ones)
-        d_fake_outputs = self.discriminator(generated_images)
-        fake_loss = self.adversarial_criterion(d_fake_outputs, zeros)
-        d_loss = real_loss + fake_loss
-        self.log('d_loss', d_loss)
-        d_opt.zero_grad()
-        self.manual_backward(d_loss)
-        d_opt.step()
 
     def validation_step(self, batch, batch_idx):
         pass
@@ -73,25 +73,25 @@ class SimpleGANModule(pl.LightningModule):
         images_grid = einops.rearrange(images_grid, 'c h w -> h w c') * 256
         self.logger.experiment.log({"images": [wandb.Image(images_grid)]})
 
-        for batch_idx in range(40):
-            w = torch.randn(250, 128, 1, 1).to(self.device)
-            generated_images = self.generator(w).cpu().numpy()
-            generated_images = \
-                einops.rearrange(generated_images, 'bs c h w -> bs h w c')
+        # for batch_idx in range(40):
+        #     w = torch.randn(250, 128, 1, 1).to(self.device)
+        #     generated_images = self.generator(w).cpu().numpy()
+        #     generated_images = \
+        #         einops.rearrange(generated_images, 'bs c h w -> bs h w c')
 
-            for image_idx, image in enumerate(generated_images):
-                cv2.imwrite(
-                    f'{self.generated_images_dir}/{batch_idx}_{image_idx}.png',
-                    image,
-                )
+        #     for image_idx, image in enumerate(generated_images):
+        #         cv2.imwrite(
+        #             f'{self.generated_images_dir}/{batch_idx}_{image_idx}.png',
+        #             image,
+        #         )
 
-        fid_score = fid.compute_fid(
-            str(self.generated_images_dir),
-            dataset_name="cifar10",
-            dataset_res=32,
-            dataset_split="test",
-        )
-        self.log('fid_score', fid_score)
+        # fid_score = fid.compute_fid(
+        #     str(self.generated_images_dir),
+        #     dataset_name="cifar10",
+        #     dataset_res=32,
+        #     dataset_split="test",
+        # )
+        # self.log('fid_score', fid_score)
 
     def configure_optimizers(self):
         g_opt = Adam(self.generator.parameters(), lr=self.lr, betas=(0.5, 0.999))
