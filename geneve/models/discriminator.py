808af03d03d1ebbebe2c51cd88f0b6cd5dc44d99
Serge Kim <sergevkim@gmail.com>
2022-11-17 17:18:04 +0300

    returned dummy disc

--- geneve/models/discriminator.py
+++ geneve/models/discriminator.py
@@ -46,6 +46,35 @@ class Discriminator(Module):
         return x
 
 
+import torch.nn as nn
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        nc = 3
+        ndf = 64
+        self.main = nn.Sequential(
+            # input is (nc) x 32 x 32
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 16 x 16
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 8 x 8
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 4 x 4
+            nn.Conv2d(ndf * 4, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid(),
+            Rearrange('bs 1 1 1 -> bs 1')
+        )
+
+    def forward(self, input):
+        return self.main(input)
+
+
 if __name__ == '__main__':
     D = Discriminator()
     images = torch.randn(8, 3, 32, 32)
